b7a0102a97a7c6b0157e9d20ab7ca74b598876fa
SriSomanaath <govindalapudisrinath@gmail.com>
2023-12-13 10:52:14 +0530

    err

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -4,12 +4,20 @@ import { useState } from "react";
 
 import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
 
-const DropDown = ({SectionName, ProductsCatalog}) => {
+interface DropDownProps {
+  SectionName: string; // Assuming SectionName is a string, update the type accordingly
+  ProductsCatalog?: any[]; // Assuming ProductsCatalog is an array, update the type accordingly
+}
 
- const [isOpen, setIsOpen] = useState(false)
+const DropDown: React.FC<DropDownProps> = ({ SectionName, ProductsCatalog }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="relative flex ">
-    <button onClick={()=> setIsOpen((prev)=> !prev)} className="flex rounded-md p-2 m-2 items-center justify-between font-bold tracking-wide border-4 border-transparent active:border-white duration-300 active:text-white">
+    <div className="relative flex">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex rounded-md p-2 m-2 items-center justify-between font-bold tracking-wide border-4 border-transparent active:border-white duration-300 active:text-white"
+      >
         {SectionName}
         {ProductsCatalog && (
           <>
@@ -19,21 +27,21 @@ const DropDown = ({SectionName, ProductsCatalog}) => {
               <AiOutlineCaretUp></AiOutlineCaretUp>
             )}
           </>
-        )}        
-    </button>
-            {ProductsCatalog && (
-                isOpen && (
-                    <div className="absolute flex-row lg:top-20 top-10 lg:right-2 right-32 flex flex-col items-center rounded-lg p-2 w-full">
-                        {ProductsCatalog.map((item, i) => (
-                            <div key={i} className="flex w-full justify-between bg-sky-500 cursor-pointer rounded-lg border-l-transparent hover:border-l-white border-l-4 m-1 hover:text-black">
-                                <h5 className="font-bold p-4 text-sm w-full">{item.title}</h5>
-                            </div>
-                        ))}
-                    </div>
-                )
         )}
+      </button>
+      {ProductsCatalog && (
+        isOpen && (
+          <div className="absolute flex-row lg:top-20 top-10 lg:right-2 right-32 flex flex-col items-center rounded-lg p-2 w-full">
+            {ProductsCatalog.map((item, i) => (
+              <div key={i} className="flex w-full justify-between bg-sky-500 cursor-pointer rounded-lg border-l-transparent hover:border-l-white border-l-4 m-1 hover:text-black">
+                <h5 className="font-bold p-4 text-sm w-full">{item.title}</h5>
+              </div>
+            ))}
+          </div>
+        )
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default DropDown
+export default DropDown;
